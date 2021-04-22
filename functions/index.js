4ecd9ca16aeae6ececba2c554930051e9eaa9ba9
Markus Wesslén <markus.wesslen@gmail.com>
2021-04-22 14:58:29 +0200

    Add onCall function

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,11 @@ const { exec } = require("child_process");
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
+exports.helloWorld = functions.region("europe-west1").https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
+});
+
+exports.callThis = functions.region("europe-west1").https.onCall((data, context) => {
+  return {message: "Hello World"}
 });
