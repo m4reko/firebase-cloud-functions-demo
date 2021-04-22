7f4cec4c7370b72a1242a00651f611e8e8d20c8d
Markus Wesslén <markus.wesslen@gmail.com>
2021-04-22 11:12:55 +0200

    Add ffmpeg dependecies

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require("firebase-functions");
+const { exec } = require("child_process");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
